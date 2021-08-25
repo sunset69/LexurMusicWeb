d265c040937fd2b327c4b86a5290ef0388abd606
lexur <2259334682@qq.com>
2021-08-25 18:02:53 +0800

    完成修改分类

--- musicweb/src/main/java/cc/lexur/controller/GenreController.java
+++ musicweb/src/main/java/cc/lexur/controller/GenreController.java
@@ -4,6 +4,7 @@ import cc.lexur.pojo.Genre;
 import cc.lexur.pojo.Msg;
 import cc.lexur.services.GenreService;
 import com.github.pagehelper.PageInfo;
+import com.sun.org.apache.bcel.internal.generic.GETFIELD;
 import org.apache.ibatis.annotations.Param;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import javax.swing.plaf.nimbus.NimbusLookAndFeel;
 import java.util.List;
 
 /**
@@ -76,5 +78,30 @@ public class GenreController {
         }
         return Msg.fail().setMsg("用户不存在");
     }
+
+    /**
+     * 修改分类
+     * @param id
+     * @param name
+     * @param description
+     * @return
+     */
+    @RequestMapping("/updateGenre")
+    @ResponseBody
+    public Msg updateGenre(@RequestParam int id,String name,String description){
+        Genre genre = new Genre();
+        genre.setId(id);
+        genre.setName(name);
+        genre.setDescription(description);
+        if (name == null || name == ""){
+            //return Msg.fail().setMsg("分类名不能为空");
+            genre.setName(null);
+        }
+        if (!genreService.updateGenre(genre)){
+            return Msg.fail().setMsg("分类不存在");
+        }
+        return Msg.success();
+    }
+
 
 }
